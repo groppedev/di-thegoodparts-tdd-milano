e0698a01911e7d729fe2e873a9b3e5a688b9ca2c
groppedev <massimo.groppelli@gmail.com>
2018-04-17 03:33:36 +0200

    Aggiunta suite di test per JUnit

---
+++ src/test/java/groppedev/dithegoodparts/test/junit/JUnitTest.java
@@ -0,0 +1,11 @@
+package groppedev.dithegoodparts.test.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SpellCheckerTest.class })
+public class JUnitTest 
+{
+
+}
